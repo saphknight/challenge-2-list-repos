b4b9bd8c42952825181ec595e010d878d672b6ba
George Liu <georgeliu96@gmail.com>
2019-06-25 22:21:30 -0400

    Start Challenge 2

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log('document ready');
 });
+
+function getRepos (username) {
+    var info = fetch(`https://www.api.github.com/users/${username}/repos`)
+
+    
+};
